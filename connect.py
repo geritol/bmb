93f87fc7164cd85be93d8e47a5d0496cd7bdd170
szepnapot <silentpocok@gmail.com>
2017-10-28 13:31:24 +0200

    pycnp import, requesting,  missing interface

--- connect.py
+++ connect.py
@@ -1,6 +1,21 @@
 import capnp
 import settings
+import Request_capnp
 
-capnp.remove_import_hook()
-request_capnp = capnp.load('./protokoll/Request.capnp')
+def lambda_print(to_print):
+    print(to_print)
+
 client = capnp.TwoPartyClient(settings.endpoint)
+request = Request_capnp.Request.new_message()
+request.login.team = settings.team
+request.login.hash = settings.hash
+
+print("Client: {}".format(client))
+print("Request: {}".format(request))
+
+# connecting
+temp_request = client.bootstrap().cast_as(request.)
+request = temp_request.convert_request()
+promise = request.send()
+promise.then(lambda ret: lambda_print(ret)).wait()
+
