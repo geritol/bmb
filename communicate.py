06682d9f5d1c809c8ee625024e881e4266d04b4d
geritol <gergo.tolnai@gmail.com>
2017-11-11 17:42:51 +0100

    added move method (takes care of moving units), added read socket data, which reads all the data from the socket (waits for the data to be complete)

--- communicate.py
+++ communicate.py
@@ -9,22 +9,80 @@ import settings
 
 TCP_DOMAIN = settings.endpoint.split(':')[0]
 TCP_PORT = int(settings.endpoint.split(':')[1])
-BUFFER_SIZE = 1000000
+BUFFER_SIZE = 1024
 
 
 def connect(loggin_info):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_DOMAIN, TCP_PORT))
-    s.send(loggin_info.to_bytes())
-    import time
-    time.sleep(1)
-    data = s.recv(BUFFER_SIZE)
 
-    res = response_schema_capnp.Response.from_bytes(data).to_dict()
+    s.send(loggin_info.to_bytes())
+    read_socket_data(s)
+
+    res = move(s, [{'unit': 0, 'direction': 'right'}])
+
     board = Board(res)
-    print(board)
+    # print(board)
+    # print(board.state)
+    print(board.state['units'])
+
+    res = move(s, [{'unit': 0, 'direction': 'down'}])
+    board = Board(res)
+    # print(board)
+    # print(board.state)
     print(str(board.evaluate()))
 
-    s.close()
-    return data
+    res = move(s, [{'unit': 0, 'direction': 'right'}])
+    board = Board(res)
+    #print(board)
+    # print(board.state)
+    print(board.state['units'])
+
+    res = move(s, [{'unit': 0, 'direction': 'right'}])
+    board = Board(res)
+    print(board)
+    # print(board.state)
+    print(board.state['units'])
+
+    while True:
+        response = move(s, [{'unit': 0, 'direction': 'down'}])
+        print("Response status: {}".format(response['status']))
+        board = Board(response)
+        print(board)
+
+
+def move(socket, action_list):
+    request_payload = request_schema_capnp.Command.new_message()
+    command = request_payload.init('commands')
+    moves_data = command.init('moves', 1)
+    for i, action in enumerate(action_list):
+        move = moves_data[i]
+        move.unit = action['unit']
+        move.direction = action['direction']
+    socket.send(request_payload.to_bytes())
+
+    response = read_socket_data(socket)
+    print("Rsponse status: {}".format(response['status']))
+    return response
+
+
+def read_socket_data(socket):
+    res = b''
+
+    while True:
+        chunk = socket.recv(BUFFER_SIZE)
+        res += chunk
+
+        finished = True
+        try:
+            # read throws error if message is not complete
+            response_schema_capnp.Response.from_bytes(res).to_dict()
+        except:
+            finished = False
+
+        if finished:
+            break
+    response = response_schema_capnp.Response.from_bytes(res).to_dict()
+    print('Response length: {}'.format(len(res)))
+    return response
