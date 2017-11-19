77ce8477d2f0999a62e26164f7bb6cace08394de
geritol <gergo.tolnai@gmail.com>
2017-11-19 18:17:04 +0100

    first working

--- next_move_rl.py
+++ next_move_rl.py
@@ -7,18 +7,78 @@ from Game import Game
 from Board import Board
 from reduce_state import reduce_state
 
+empty_state = [[{}, {}, {}, {}, {}, {}, {}],
+                [{}, {}, {}, {}, {}, {}, {}],
+                [{}, {}, {}, {}, {}, {}, {}],
+                [{}, {}, {}, {}, {}, {}, {}],
+                [{}, {}, {}, {}, {}, {}, {}],
+               ]
+
 def move(state, action):
     next_board = Game(Board(state)).move(action)
-    score = next_board.evaluate()
-    done = False if score else True
-    return next_board['state'], score, done, None
+    print(next_board)
+    if next_board:
+        score = next_board.evaluate()
+        return convert_to_list(reduce_state(next_board.state)[action[0]['unit']]), score, False, None
+    else:
+        score = -10000
+        return convert_to_list(empty_state), score, True, None
 
 
-state_size = 5000
+def convert_to_list(state):
+    res = []
+    for line in state:
+        for cell in line:
+            if 'attack' in cell:
+                if 'can' in cell['attack']:
+                    res.append(1)
+                else:
+                    res.append(0)
+                if 'unit' in cell['attack']:
+                    res.append(cell['attack']['unit'])
+                else:
+                    res.append(10000)
+            else:
+                res.append(10000)
+                res.append(10000)
+            if 'owner' in cell:
+                res.append(cell['owner'])
+            else:
+                res.append(10000)
+            if 'unit' in cell:
+                res.append(1)
+            else:
+                res.append(10000)
+            if 'enemy' in cell:
+                res.append(1)
+                res.append(directions_back[cell['enemy']['direction']['horizontal']])
+                res.append(directions_back[cell['enemy']['direction']['vertical']])
+            else:
+                res.append(10000)
+                res.append(10000)
+                res.append(10000)
+    return res
+
+
+state_size = 245
 action_size = 4
 agent = DQNAgent(state_size, action_size)
-# agent.load("./save/xonix-ddqn.h5")
+agent.load("./save/xonix-ddqn.h5")
 batch_size = 32
+
+derections = {
+    0: 'up',
+    1: 'left',
+    2: 'down',
+    3: 'right'
+}
+
+directions_back = {
+    'up': 0,
+    'left': 1,
+    'down': 2,
+    'right': 3
+}
 
 
 def next_move(state):
@@ -29,22 +89,23 @@ def next_move(state):
     for i, reduced_state in enumerate(reduced_states):
         unit_number = i
 
-        action = agent.act(reduced_state)
+        r_state = np.reshape(convert_to_list(reduced_state), [1, state_size])
+        action = agent.act(r_state)
+        action = derections[action]
         print('action:', action)
-        next_state, reward, done, _ = move(reduced_state, action)
+        next_state, reward, done, _ = move(state, [{'unit': unit_number, 'direction': action}])
         next_state = np.reshape(next_state, [1, state_size])
-        agent.remember(state, action, reward, next_state, done)
+        agent.remember(r_state, directions_back[action], reward, next_state, done)
 
-        moves.append({'unit': unit_number, 'direction': 'right'})
+        moves.append({'unit': unit_number, 'direction': action})
 
         if done:
             agent.update_target_model()
             print("e: {:.2}"
                   .format(agent.epsilon))
-            break
+            agent.save("./save/xonix-ddqn.h5")
 
         if len(agent.memory) > batch_size:
             agent.replay(batch_size)
-            # if e % 10 == 0:
-            #     agent.save("./save/xonix-ddqn.h5")
+            agent.save("./save/xonix-ddqn.h5")
     return moves
